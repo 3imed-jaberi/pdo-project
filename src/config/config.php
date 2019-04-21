3c1135701f753360ec7982c44673a63195eb7b4e
unknown <imed_jebari@hotmail.fr>
2019-04-22 00:11:49 +0200

    the global config data ..

---
+++ src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '22413578');
+   define('DB_NAME', 'mypdo');
+   define('TABLE_NAME', 'users');
+   define('URL','http://localhost/app/');
